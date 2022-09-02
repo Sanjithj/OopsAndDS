3521bc652690a8af2a3b6eb2c42145d7a5a03c2f
Sanjithj <sanjithj591997@gmail.com>
2022-09-02 21:37:53 +0530

    Added: Unique deck of cards

--- DeckOfCards.java
+++ DeckOfCards.java
@@ -1,7 +1,59 @@
 package com.bridgelabz;
 
 public class DeckOfCards {
-public static void main(String[] args) {
+ 
+	public static void main(String[] args) {
 	System.out.println("Welcome to deck of Cards");
+	}
+		public static final String[] RANKS = {"2", "3", "4", "5", "6", "7","8", "9", "10", "Jack", "Queen", "King","Ace"};
+
+	    public static final String[] SUITS = {"Clubs", "Diamonds", "Hearts", "Spades"};
+
+	    private final int rank;
+
+	    private final int suit;
+
+	    public DeckOfCards(int rank, int suit) {
+	        this.rank = rank;
+	        this.suit = suit;
+	    }
+
+	    public int compareTo(DeckOfCards that) {
+	        if (this.suit < that.suit) {
+	            return -1;
+	        }
+	        if (this.suit > that.suit) {
+	            return 1;
+	        }
+	        if (this.rank < that.rank) {
+	            return -1;
+	        }
+	        if (this.rank > that.rank) {
+	            return 1;
+	        }
+	        return 0;
+	    }
+	    public boolean equals(DeckOfCards that) {
+	        return this.rank == that.rank
+	            && this.suit == that.suit;
+	    }
+
+	    
+	    public int getRank() {
+	        return this.rank;
+	    }
+
+	    
+	    public int getSuit() {
+	        return this.suit;
+	    }
+
+	    public int position() {
+	        return this.suit * 13 + this.rank - 1;
+	    }
+
+	    public String toString() {
+	        return RANKS[this.rank] + " of " + SUITS[this.suit];
+	       
 }
 }
